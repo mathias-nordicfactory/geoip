d1779acab33b3de271a8705bba8387c2953c41f7
unknown <mathias.wilen@hotmail.com>
2015-10-09 13:51:46 +0200

    another CORS test

--- app.js
+++ app.js
@@ -45,9 +45,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 console.log("app started")
 
-app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
  });
 

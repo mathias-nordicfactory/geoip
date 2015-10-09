3c1a57eed8cf430ad4157eb6cab7c9a0ad0e68c8
unknown <mathias.wilen@hotmail.com>
2015-10-09 13:48:24 +0200

    enabled CORS

--- app.js
+++ app.js
@@ -45,6 +45,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 console.log("app started")
 
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+ });
+
 app.get('/', function (req, res, next) {
     res.render('index', {
         title: "IP GEOLOCATION"
